2bf0696716b42d8dffa72b9221086a259dec20ab
TAxford <t.axford2@yahoo.com>
2018-03-07 10:38:01 -0500

    added director and movie class

---
+++ MovieDirectorApp/Models/Director.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieDirectorApp.Models
+{
+    public class Director
+    {
+        [Key]
+        public int DirectorID { get; set; }
+        public string Name { get; set; }
+
+        //Navagation Property
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
