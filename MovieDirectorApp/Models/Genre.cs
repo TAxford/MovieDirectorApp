100dea34c85e35c8d2a296959945afb1e79d19ad
TAxford <t.axford2@yahoo.com>
2018-03-07 10:40:10 -0500

    added Genre class

---
+++ MovieDirectorApp/Models/Genre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieDirectorApp.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreID { get; set; }
+        public string Type { get; set; }
+
+
+
+
+        //Navagation Property
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
